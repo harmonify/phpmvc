900dcb3cd54e5915c062dc648a23f69cbf2d41bf
Wendy Surya Wijaya <wendy18.sw@gmail.com>
2021-08-02 23:26:37 +0700

    finished Flasher class and the documentation

--- app/core/Flasher.php
+++ app/core/Flasher.php
@@ -1,11 +1,17 @@
 <?php
-
 class Flasher {
-  public static function setFlash($message, $action, $type)
+/**
+ *
+ * @param mixed $message action taken
+ * @param mixed $condition adjective
+ * @param mixed $type alert type
+ * @return alert You have $condition $message
+ */
+  public static function setFlash($message, $condition, $type)
   {
     $_SESSION['flash'] = [
       'message' => $message,
-      'action' => $action,
+      'condition' => $condition,
       'type' => $type
     ];
   }
@@ -13,7 +19,11 @@ class Flasher {
   public static function flash()
   {
     if (isset($_SESSION['flash'])) {
-      echo "";
+      echo "<div class=\"alert alert-{$_SESSION['flash']['type']} alert-dismissible fade show\" role=\"alert\">
+              You have <strong>{$_SESSION['flash']['condition']}</strong> {$_SESSION['flash']['message']}
+              <button type=\"button\" class=\"btn-close\" data-bs-dismiss=\"alert\" aria-label=\"Close\"></button>
+            </div>";
+      unset($_SESSION['flash']);
     }
   }
 }
