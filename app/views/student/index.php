f2736942ca8228e868c84ba9df31f8911d9b32e9
Wendy Surya Wijaya <wendy18.sw@gmail.com>
2021-08-03 14:51:39 +0700

    added delete button and confirmation modal

--- app/views/student/index.php
+++ app/views/student/index.php
@@ -7,7 +7,7 @@
   </div>
   <!-- Button to trigger modal -->
   <div class="row mb-3">
-    <div class="col-2">
+    <div class="col-3">
       <button type="button" class="btn btn-primary w-100" data-bs-toggle="modal" data-bs-target="#formModal">
         <i class="bi bi-plus mr-2"></i>
         <span>Add New Student</span>
@@ -32,7 +32,13 @@
           <tr>
             <th scope="row"><?= $i++ ?></th>
             <td><?= $student['name'] ?></td>
-            <td><a href="<?= BASEURL ?>/student/detail/<?= $student["id"] ?>" class="badge bg-dark">Detail</a></td>
+            <td class="text-white">
+              <a href="<?= BASEURL ?>/student/detail/<?= $student["id"] ?>"
+                class="badge bg-dark text-reset text-decoration-none">Detail</a>
+              <a href="<?= BASEURL ?>/student/delete/<?= $student["id"] ?>"
+                class="badge bg-danger text-reset text-decoration-none" data-bs-toggle="modal"
+                data-bs-target="#deleteModal" onclick="passData('<?= $student['name'] ?>')">Delete</a>
+            </td>
           </tr>
           <?php endforeach; ?>
         </tbody>
@@ -41,7 +47,7 @@
   </div>
 </div>
 
-<!-- Modal -->
+<!-- Form Modal -->
 <div class="modal fade" id="formModal" tabindex="-1" aria-labelledby="formModalLabel" aria-hidden="true">
   <div class="modal-dialog modal-dialog-centered">
     <div class="modal-content">
@@ -78,3 +84,28 @@
     </div>
   </div>
 </div>
+
+<!-- Delete Modal -->
+<div class="modal fade" id="deleteModal" tabindex="-1" aria-labelledby="deleteModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="deleteModalLabel">Confirm Delete</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        Are you sure to delete the student with the name <span id="student-name"></span> ?
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Confirm</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+<script defer>
+  function passData(data) {
+    document.querySelector('#student-name').innerText = data;
+  }
+</script>
