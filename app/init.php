fffc54e4b98b5b2ed7cacce02d97f6fd9066bc64
Wendy Surya Wijaya <wendy18.sw@gmail.com>
2021-08-06 19:36:30 +0700

    autoload the core files

--- app/init.php
+++ app/init.php
@@ -1,12 +1,7 @@
-<?php 
-
-require_once "core/App.php";
-require_once "core/Controller.php";
-require_once "core/Database.php";
-require_once "core/Flasher.php";
+<?php
 
 require_once "config/config.php";
 
-// spl_autoload_register(function ($class) {
-//   require_once "core/{$class}.php";
-// });
+spl_autoload_register(function ($class) {
+  require_once "core/{$class}.php";
+});
